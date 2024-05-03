20607768c36fe75ed3b3423707e9c1485b10dde2
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2024-05-03 12:06:01 +0530

    Update wts_previous_month.py

--- wts_previous_month.py
+++ wts_previous_month.py
@@ -1,34 +1,27 @@
-def pull_files(self):
+import os
+import glob
+import datetime
+import logging
+
+def pull_previous_month_files(self):
     try:
-        logging.info('Came to pull_files method')
-        # Calculate the year and month of the previous month
-        today = datetime.datetime.today()
-        if today.month == 1:
-            prev_month = today.replace(year=today.year - 1, month=12)
-        else:
-            prev_month = today.replace(month=today.month - 1)
+        logging.info('Came to pull_previous_month_files method')
+        current_year = datetime.datetime.today().year
+        current_month = datetime.datetime.today().month
+        previous_month = current_month - 1 if current_month > 1 else 12
+        previous_year = current_year if current_month > 1 else current_year - 1
         
-        prev_month_str = prev_month.strftime('%Y-%m')
-
-        # Construct the path to the directory containing the files to be pulled
         files_path = os.path.join('//Sdcfsvp03/data03/1792/KPI/Transactions/Monthly WTS Trade Data/', '*.txt')
-
-        # Find all files matching the specified pattern in the directory
         files = sorted(glob.iglob(files_path), key=os.path.getctime, reverse=True) 
         logging.info('WTS files_path: '+ str(files_path))
-
         foundFileList = []
 
         for file in files:
-            # Extract the file name without the extension
-            file_name = os.path.basename(file).split('.')[0]
-
-            # Check if the file belongs to the previous month
-            if file_name.startswith(prev_month_str):
+            if file.find(f'{previous_year}-{previous_month:02}', 0, len(file)) > 0:
+                # It's from the previous month of the current year
                 foundFileList.append(file) 
-
-        logging.info('WTS filelist:'+str(foundFileList))
+        logging.info('Previous month WTS filelist:'+str(foundFileList))
         return foundFileList
     except Exception as e:
-        logging.error('Error in pull_files method: {}'.format(str(e)))
+        logging.error('Error in pull_previous_month_files method: {}'.format(str(e)))
         return None
