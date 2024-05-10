fa35069965ec72f2d735bcb25eb4fe2412e8eafa
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2024-05-10 15:49:37 +0530

    Update sam_sec.py

--- sam_sec.py
+++ sam_sec.py
@@ -1,25 +1,49 @@
-data = [file for file in data if sunday_date in file and 'Sec_Cond' in file]
+try:
 
-logging.info('Security Detail -get_data method data is + str(data))
+logging.info('Security Detail Came to try block')
 
-for line in data:
+logging.info('Security Detail line -131 who is + str(who))
 
-col line.split()
+for key in who:
 
-datestr'.join(line.split() [0:2])
+logging.info('Security Detail line -133 key is + str(key)) f.retrbinary('RETR %s' % who [key], open (who [key], 'wb').write)
 
-date time.strptime (datestr, '%m-%d-%y %H:%M%p')
+logging.info('Security Detail line -138 file going to before quit')
 
-datelist.append(date)
+f.quit()
 
-logging.info('Security Detail File names are is+ str(col[3]))
+logging.info('Security Detail file going to after quit')
 
-filelist.append(col[3])
+if f.close:
 
-logging.info('Security Detail -get_data method filelist is: '+ str(filelist))
+logging.info('Security Detailif file closed')
 
-combo zip(datelist, filelist)
+else:
 
-logging.info('Security Detail line -126 combo is: '+ str(combo))
+logging.info('Security Detail else file is NOT closed')
 
-who = dict(combo) logging.info('Security Detail line -128 who is + str(who))
+logging.info('Security Detail file completed last line')
+
+logging.info('Security Detail Came to try block_step_1')
+
+files_path= os.path.join(os.getcwd(), sunday_date + '*Sec_Cond*.xls')
+
+logging.info('Security Detail -get_data method line 151 files_path is_step_2: '+ str(files_path))
+
+files sorted (
+
+glob.iglob(files_path), key=os.path.getctime, reverse=True)
+
+logging.info('Security Detail -get_data method line-154 files is_step_3: '+ str(files)) except Exception as e:
+
+logging.info('Security Detail Came to except block')
+
+logging.info('Security Detail exception is: '+ str(e))
+
+logging.exception('Exception on downloading Security Detail files')
+
+self.batch_log_error(str(e))
+
+logging.exception('Security Detail Came to except block going to RETURN something')
+
+return
