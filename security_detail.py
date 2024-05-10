bfa71f9c14f62aa8667ee6df58f72831387b3c73
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2024-05-10 15:16:16 +0530

    Update security_detail.py

--- security_detail.py
+++ security_detail.py
@@ -17,32 +17,34 @@ import sys
 
 class SecurityDetail:
   
-  def __init__(self,batch_id,params):
-    self.batch_id = batch_id
-    self.params = params
-    logging.getLogger().setLevel (logging.INFO)
-    self.error_status = False
+    def __init__(self,batch_id,params):
+        self.batch_id = batch_id
+        self.params = params
+        logging.getLogger().setLevel (logging.INFO)
+        self.error_status = False
 
-  def batch_log_success(self):
-    logging.info('Started Security Detail came to batch_log_success method')
-    engine = create_engine('mssql+pyodbc:///?odbc_connect=%s' % self.params, fast_executemany=True)
+    def batch_log_success(self):
+        logging.info('Started Security Detail came to batch_log_success method')
+        engine = create_engine('mssql+pyodbc:///?odbc_connect=%s' % self.params, fast_executemany=True)
 
-    batch_id = str(self.batch_id)
-    logging.info('Started Security Detail came to batch_log_success method batch_id: '+ str(batch_id))
-    
-    exec_string = """
-    DECLARE @curDate datetime2(7)
-    SET @curDate = CURRENT_TIMESTAMP
-    UPDATE [daedbo].[dae_fabi_batch_log] set s_batch_end_date = @curDate where i_batch_id = '"""" + batch_id + """'
-    UPDATE [daedbo].[dae_fabi_batch_log] set t_batch_status 'SUCCESS' where i_batch_id = '"""" + batch_id + """'
-    """
-    try:
-      connection engine.raw_connection()
-      cursor = connection.cursor()
-      cursor.execute(exec_string)
-      cursor.commit()
-      connection.close()
-      except Exception as e:
-      logging.info('tarted Security Detail came to batch_log_success method exception is: '+ str(e))
-      logging.exception('Exception on batch log success update')
-      return
+        batch_id = str(self.batch_id)
+        logging.info('Started Security Detail came to batch_log_success method batch_id: '+ str(batch_id))
+
+        exec_string = """       
+        DECLARE @curDate datetime2(7)
+        SET @curDate = CURRENT_TIMESTAMP
+        UPDATE [daedbo].[dae_fabi_batch_log] set s_batch_end_date = @curDate where i_batch_id = '"""" + batch_id + """'
+        UPDATE [daedbo].[dae_fabi_batch_log] set t_batch_status 'SUCCESS' where i_batch_id = '"""" + batch_id + """'
+        """
+        
+
+        try:
+            connection = engine.raw_connection()
+            cursor = connection.cursor()
+            cursor.execute(exec_string)
+            cursor.commit()
+            connection.close()
+        except Exception as e:
+            logging.info('Started Security Detail came to batch_log_success method exception is: '+ str(e))
+            logging.exception('Exception on batch log success update')
+        return
